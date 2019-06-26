2fd8f9974bb6a37a942a2a9821dafda85c5b2a97
Anubhav652 <anubhavagarwal033@gmail.com>
2019-06-27 00:12:31 +0530

    Expansion
    Expansion

--- pokeconfig.py
+++ pokeconfig.py
@@ -1,2 +1,6 @@
-class pokeconfig:
-	token = ""
+class PokeConfig:
+    token = ""
+    rewardsTimer = 1 * 60 * 60 * 24  # 24 hours cooldown between daily rewards
+    plrReward = 500  # Daily reward amount is set here
+    startingGold = 500  # Gold given when a player starts
+    startingPokemon = [1, 4, 7]  # 1 = Bulbasaur, 4 = Charmander, 7 = Squirtle
