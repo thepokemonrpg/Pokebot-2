c772059cd7ce963836bf303a419f45df0a7337d1
Anubhav652 <anubhavagarwal033@gmail.com>
2019-06-27 06:02:54 +0530

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -3,41 +3,49 @@ import pokepy
 
 client = pokepy.V2Client()
 
-def get_pokemon_name(pokemon_name):
-    pokemon = client.get_pokemon(pokemon_name)
-    return pokemon.name
 
-def get_pokemon_abilities(pokemon_name):
-    pokemon = client.get_pokemon(pokemon_name)
-    list = []
-    abilities = pokemon.stats
-    length = len(abilities)
-    for i in range(length):
-        list.append(abilities[i].stat.name)
+class BadArgumentError(Exception):
+    pass
 
-    return list
 
-def get_pokemon_moves(pokemon_name):
-    pokemon = client.get_pokemon(pokemon_name)
-    return pokemon.moves
+class Pokemon:
+    def __init__(self, **kwargs):
+        if kwargs.get("name") is str:
+            self.pokemon = client.get_pokemon(kwargs.get("name"))
+        elif kwargs.get("id") is int:
+            self.pokemon = client.get_pokemon(kwargs.get("id"))
+        else:
+            raise BadArgumentError("Invalid arguments passed on")
 
-def get_pokemon_stats(pokemon_name):
-    pokemon = client.get_pokemon(pokemon_name)
-    return pokemon.stats
+        self.stats = []
+        statistics = self.pokemon.stats
+        statsLength = len(statistics)
+        for i in range(statsLength):
+            self.stats.append(statistics[i].stat.name)
 
-def get_pokemon_cost(pokemon_name):
-    pokemon = client.get_pokemon(pokemon_name)
-    abilities = pokemon.stats
-    length = len(abilities)
-    total = 0
-    for i in range(length):
-        total += abilities[i].base_stat
+        self.moves = self.pokemon.moves
 
-    return total*2
+        self.stats = self.pokemon.stats
 
-def get_pokemon_potential_abilities(pokemon_name):
-    pokemon = client.get_pokemon(pokemon_name)
-    return pokemon.abilities
+        self.cost = 0
 
-if __name__ == '__main__':
-    get_pokemon_cost("ditto")
+        for i in range(statsLength):
+            self.cost += statistics[i].base_stat
+        self.cost = self.cost * 2
+
+        self.abilities = self.pokemon.abilities
+
+    def get_pokemon_name(self):
+        return self.pokemon.name
+
+    def get_pokemon_moves(self):
+        return self.moves
+
+    def get_pokemon_stats(self):
+        return self.stats
+
+    def get_pokemon_cost(self):
+        return self.cost
+
+    def get_pokemon_potential_abilities(self):
+        return self.abilities
