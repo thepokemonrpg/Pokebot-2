a21130cf73d7d6f4a65b6a6e0c8cca1299ebd2d1
NikosGrigoratos <nick1038@gmail.com>
2019-06-27 19:14:26 +0300

    more classes

--- Pokeball.py
+++ Pokeball.py
@@ -1,9 +1,17 @@
+import random
 from Item import Item
+
 
 
 class Pokeball(Item):
     def __init__(self, **kwargs):
         Item.__init__(self, **kwargs)
+
+    def calc_chance(self):
+        if random.randint(0,100) < 50:
+            print("catches")
+        else:
+            print("doesn't catch")
 
 
 
