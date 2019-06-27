f12cf89d3beddf6bcf974938216d4f16a9d5c878
Anubhav652 <anubhavagarwal033@gmail.com>
2019-06-27 13:47:15 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ def get_connection():
 	cur = con.cursor()
 	cur.execute("DROP TABLE playerData")
 	cur.execute("CREATE TABLE IF NOT EXISTS playerData(playerID TEXT PRIMARY KEY, gold INTEGER, rewardsEpoch INT, startRewardsObtained INT)")
-	cur.execute("CREATE TABLE IF NOT EXISTS playerData(pokemonID TEXT PRIMARY KEY, pokemonIdentifier INTEGER, level INT, owner TEXT)")
+	cur.execute("CREATE TABLE IF NOT EXISTS pokemon(pokemonID INT PRIMARY KEY AUTOINCREMENT, pokemonIdentifier INT, level INT, owner TEXT, moves TEXT, bonus INT)")  
+        cur.execute("CREATE TABLE IF NOT EXISTS items(item INT AUTOINCREMENT, itemName TEXT, itemDesc TEXT, itemS my INT) 
 	con.commit()
 	return con
