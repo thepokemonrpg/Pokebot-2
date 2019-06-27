4684f0f5aad2af218238c7f2539cb8d3bedacc9c
Anubhav652 <anubhavagarwal033@gmail.com>
2019-06-27 22:24:30 +0530

    Update Item.py

--- Item.py
+++ Item.py
@@ -3,34 +3,42 @@ from pokemon import BadArgumentError
 
 client = pokepy.V2Client()
 
+
 class Item:
     def __init__(self, **kwargs):
-        if kwargs.get("item_id").isdigit():
+        if kwargs.get("id").isdigit():
             self.item = client.get_item(kwargs.get("item_id"))
-            self.item_name = self.item.name
+            self.name = self.item.name
         else:
             raise BadArgumentError("Invalid arguments passed on")
 
-        if kwargs.get("item_name") is str:
-            self.item_name = kwargs.get("item_name")
+        if kwargs.get("name") is str:
+            self.name = kwargs.get("item_name")
 
         self.item_cost = self.item.cost
+        self.uses = False
+        self.desc = ""
+
+        if kwargs.get("cost"):
+            self.cost = kwargs.get("cost")
+
+        if kwargs.get("uses"):
+            self.uses = kwargs.get("uses")
+
+        if kwargs.get("description"):
+            self.desc = kwargs.get("description")
 
     def get_id(self):
         return self.item.id
 
     def get_name(self):
-        return self.item.item_name
+        return self.name
 
     def get_cost(self):
-        return self.item.item_cost
+        return self.cost
 
+    def get_use(self):
+        return self.uses
 
-
-
-
-
-
-
-
-
+    def get_description(self):
+        return self.desc
