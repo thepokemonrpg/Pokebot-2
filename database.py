2aa11784a92307e96e3c02aff195afbf9cf847cb
Anubhav652 <anubhavagarwal033@gmail.com>
2019-06-29 12:24:08 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,8 +4,8 @@ import sqlite3
 def get_connection():
 	con = sqlite3.connect("database.db")
 	cur = con.cursor()
-	cur.execute("CREATE TABLE IF NOT EXISTS playerData(playerID TEXT PRIMARY KEY, gold INTEGER, rewardsEpoch INTEGER, startRewardsObtained INTEGER)")
-	cur.execute("CREATE TABLE IF NOT EXISTS pokemon(pokemonID INTEGER PRIMARY KEY, pokemonIdentifier INTEGER, level INTEGER, owner TEXT, moves TEXT, bonus INTEGER)")
+	cur.execute("CREATE TABLE IF NOT EXISTS playerData(playerID TEXT PRIMARY KEY, gold INTEGER, rewardsEpoch INTEGER, startRewardsObtained INTEGER, equipPokemon INTEGER)")
+	cur.execute("CREATE TABLE IF NOT EXISTS pokemon(pokemonID INTEGER PRIMARY KEY, pokemonIdentifier INTEGER, level INTEGER, exp INTEGER, owner TEXT, moves TEXT, bonus INTEGER)")
 	cur.execute("CREATE TABLE IF NOT EXISTS items(item INTEGER PRIMARY KEY, owner TEXT, itemName TEXT, itemDesc TEXT, itemAmt INTEGER)")
 	con.commit()
 	return con
